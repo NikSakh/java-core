85277e595cfbc8fbfbd22d301b95dc97b150b275
NikSakh <NikSakharow@gmail.com>
2025-04-14 22:15:02 +0300

    initial commit

---
+++ src/main/java/ru/mentee/power/variables/VariableExample.java
@@ -0,0 +1,10 @@
+package ru.mentee.power.variables;
+
+public class VariableExample {
+    public static void main(String[] args) {
+
+        int myAge = 25;
+        String myName = "Никита";
+        System.out.println("Меня зовут " + myName + ", мне " + myAge + " лет.");
+    }
+}
